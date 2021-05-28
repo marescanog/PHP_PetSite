7faf4bd9627a8bb81603fd86f79c7b70b95d7255
marescanog <marescanog@gmail.com>
2021-05-28 15:11:22 +0800

    Added testimony page and htaccess, created mobile friendly version, minor adjustments lacking

--- php-html-blocks/no-results.php
+++ php-html-blocks/no-results.php
@@ -1,6 +1,6 @@
 <div class="nr__wrapper">
     <div class="nr__container">
-        <img src="./images/ICONS/no-results/Cat.svg" alt="no results icon" >
+        <img src="<?php echo './images/ICONS/no-results/'.$animal.'.svg'?>" alt="no results icon" >
         <h1>We couldn't find any pet matches</h1>
         <p>We know they're out there somewhere! Try widening your search by adjusting your filters.</p>
     </div>
---
+++ testimony.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>CEBU PET MATCH</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./css/reset-styles.css">
+    <link rel="stylesheet" href="./css/stylish-page-wrapper-styles.css">
+    <link rel="stylesheet" href="./css/testimonials-styles.css">
+    <link rel="stylesheet" href="./css/header-styles.css">
+    <link rel="stylesheet" href="./css/footer-styles.css">
+    <meta name="currentPage" content="TESTIMONIALS">
+    <meta property="og:image" content="./images/LOGO/LOGO-BLOCK.svg" />
+    <?php require './php-html-blocks/favicon.php'?>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300&display=swap');
+    </style>
+</head>
+<body >
+    <?php //require './php-html-blocks/header.php'?>
+        <div style="background-color:#766F9B; min-height:100vh;">
+            <div class="header-padding3" style="transform: translateY(15px);"></div>
+            <div class="sty__left-right-margins tes__font">
+                <div class="sty__header">
+                    <?php include './images/LOGO/headerTitles/testimonials.svg';?>
+                </div>
+                <div class="sty__-paper">
+                    <!-- Section 1-->
+                    <div class="tes_-inner-wrapper">
+                        <div class='tes_--section-1'>
+                            <div class='tes_---section-1-image'>
+                                <img src="./images/PEOPLE/testimonials/1-400x450.jpg" alt="Melinda and Roger witting on the grass with sparky">
+                            </div>
+                            <div class='tes_---section-1-content'>
+                                <h2>Melinda and Roger</h2>
+                                <div class="tes_----section-1-horizontal"></div>
+                                <div class="tes_----section-1-paragraphs">
+                                    <h3>"Our days have been filled  with more love and laughter ever since sparky came into our lives"</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Section 2-->
+                    <div class="tes_-middle-inner-wrapper">
+                        <h3>"We would go for long walks in the park. Spending time together with sparky has kept Roger and I closer"</h3>
+                        <div class='tes_--section-2-image'>
+                            <img src="./images/PEOPLE/testimonials/1-600x400.jpg" alt="">
+                        </div>
+                        <div>
+                            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco </p>
+                            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse </p>
+                        </div>
+                  </div>
+                    <!-- Section 3-->
+                    <div class="tes_-inner-wrapper">
+                            <div class='tes_--section-3'>
+                                <div class='tes_---section-3-image'>
+                                    <img src="./images/PEOPLE/testimonials/1-400x300.jpg" alt="">
+                                </div>
+                                <div class='tes_---section-3-content'>
+                                    <h2>Sparky</h2>
+                                    <div class="tes_----section-3-horizontal"></div>
+                                    <div class="tes_----section-3-paragraphs">
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse </p>
+                                        <h4>Want a pet like sparky?</h4>
+                                        <button>SEARCH FOR PET</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    
+    <script src=""></script>
+
+    <script type="text/javascript">
+    </script>
+
+</body>
+</html>
