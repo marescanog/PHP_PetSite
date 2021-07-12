50b298fd555820075a83faa63646e9710dc8c9d1
MarvieG <marviegasataya@gmail.com>
2021-07-12 10:37:12 +0800

    adjusted header css html and javascript

--- javascript/admin/header-admin.js
+++ javascript/admin/header-admin.js
@@ -1,12 +1,18 @@
 // Runtime Code
 var mobileScreen = window.matchMedia("(max-width:900px)");
 var drawer = document.getElementById("drawer");
+var navItems = document.getElementById("menuNav").children;
 
 // Reset to default when breakpoint for mobile has been crossed
 function resetDefault(mobileScreen){
     if(drawer.classList.contains("show")){
         drawer.classList.remove("show");
     }
+
+    if(document.getElementById("emptyHead").classList.contains("shrink")){
+        document.getElementById("emptyHead").classList.remove("shrink");
+    }
+
 }
 
 // listens for a change in window size
@@ -14,7 +20,19 @@ mobileScreen.addEventListener('change', resetDefault);
 
 
 function toggleDrawer(){
-    document.getElementById("drawer").classList.toggle("show");
+    drawer.classList.toggle("show");
+    document.getElementById("emptyHead").classList.toggle("shrink");
 }
+
+// Sets the background color for slected menu item
+function setSelectedMenuItem(num){
+    if(num == navItems.length || num < 0){
+        console.log("Out of Range, check setSelectedMenu Items in header admin javascript");
+    } else {
+        navItems[num].style.backgroundColor = "#423e57";
+    }
+}
+
+
 
 
--- php-html-blocks/admin/header-admin.php
+++ php-html-blocks/admin/header-admin.php
@@ -20,12 +20,13 @@
                     <h4>MENU</h4>
                     <svg onclick="toggleDrawer()" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>
                 </div>
-                <ul>
-                    <li style="background-color:#423e57">DASHBOARD</li>
-                    <li>ALL TICKETS</li>
-                    <li>PETS DATABASE</li>
-                    <li>USER REQUESTS</li>
-                    <li>TETIMONIALS</li>
+                <ul id="menuNav">
+                    <!-- style="background-color:#423e57" -->
+                    <li onclick="window.location='./dash.php'"><a href="./dash.php">DASHBOARD</a></li>
+                    <li onclick="window.location='./tickets.php'"><a href="./tickets.php">ALL TICKETS</a></li>
+                    <li onclick="window.location='./petsdatabase.php'"><a href="./petsdatabase.php">PETS DATABASE</a></li>
+                    <li onclick="window.location='./general.php'"><a href="./general.php">USER REQUESTS</a></li>
+                    <li onclick="window.location='./editcontent.php'"><a href="./editcontent.php">TESTIMONIALS</a></li>
                 </ul>
             </div>
         </div>
